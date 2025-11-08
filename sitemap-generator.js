2786628ddeb48ae53266ad8200498c3d188278ab
devtechcoder <devtechcoder@gmail.com>
2025-11-08 15:26:15 +0530

    update site map file

--- sitemap-generator.js
+++ sitemap-generator.js
@@ -10,15 +10,24 @@ require("@babel/register")({
   ],
 });
 
+const React = require("react");
+const { Route } = require("react-router-dom");
 const { publicRoutes } = require("./src/routes.js");
 const Sitemap = require("react-router-sitemap").default;
 const { createWriteStream } = require("fs");
 const { resolve } = require("path");
 
 function generateSitemap() {
-  const paths = publicRoutes.map((route) => route.path.replace(/:[^/]+/g, ""));
-  const sitemap = new Sitemap(paths.map((path) => ({ id: path, path }))).build("https://blackdiary.vercel.app").toString();
+  // Create a Route component from your public routes array
+  const routes = (
+    <Route>
+      {publicRoutes.map((route, index) => (
+        <Route key={index} path={route.path} />
+      ))}
+    </Route>
+  );
 
+  const sitemap = new Sitemap(routes).build("https://blackdiary.vercel.app").toString();
   const dest = resolve(__dirname, "build", "sitemap.xml");
   createWriteStream(dest).write(sitemap);
   console.log(`Sitemap generated at ${dest}`);
