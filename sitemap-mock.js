42f6597c7bc680005cd66275e0bb9ba0d238517a
devtechcoder <devtechcoder@gmail.com>
2025-11-11 01:21:41 +0530

    add explore shayari page

---
+++ sitemap-mock.js
@@ -0,0 +1,2 @@
+// This is an empty module to mock browser-only libraries during sitemap generation.
+module.exports = {};
--- sitemap-runner.js
+++ sitemap-runner.js
@@ -2,12 +2,24 @@ require("@babel/register")({
   presets: ["@babel/preset-env", "@babel/preset-react"],
   plugins: [
     [
+      "babel-plugin-module-resolver",
+      {
+        alias: {
+          // Mock browser-only modules for sitemap generation
+          "react-draft-wysiwyg": "./sitemap-mock.js",
+          "rc-image": "./sitemap-mock.js",
+          "react-quill": "./sitemap-mock.js",
+        },
+      },
+    ],
+    [
       "transform-assets",
       {
         extensions: ["css", "scss", "sass", "png", "jpeg", "jpg", "gif", "svg"],
       },
     ],
   ],
+  ignore: [/node_modules\/(?!react-router-sitemap)/],
 });
 
 require("./sitemap-generator.js");
